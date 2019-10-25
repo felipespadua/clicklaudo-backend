5a43b3c20cb616697a50eb372b534d845d54714c
Felipe Padua <felipepadua@Mariannas-MBP.localdomain>
2019-10-25 13:01:34 -0300

    socket funcionando

--- app.js
+++ app.js
@@ -54,5 +54,45 @@ app.locals.title = 'Express - Generated with IronGenerator';
 const index = require('./routes/index');
 app.use('/', index);
 
+app.use((req, res, next) => {
+  res.status(404);
+  res.render('not-found');
+});
+
+app.use((err, req, res, next) => {
+  // always log the error
+  console.error('ERROR', req.method, req.path, err);
+
+  // only render if the error ocurred before sending the response
+  if (!res.headersSent) {
+    res.status(500);
+    res.render('error');
+  }
+});
+
+
+const http = require('http');
+let server = http.createServer(app);
+
+
+server.on('error', error => {
+  if (error.syscall !== 'listen') { throw error }
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(`Port ${process.env.PORT} requires elevated privileges`);
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(`Port ${process.env.PORT} is already in use`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+});
+
+
 
 module.exports = app;
