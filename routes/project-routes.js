069d7559475575386a8117ca21f21d679968cb1b
Felipe Padua <felipepadua@Mariannas-MBP.localdomain>
2019-11-01 00:05:49 -0300

    deleteexam

--- routes/project-routes.js
+++ routes/project-routes.js
@@ -128,16 +128,43 @@ router.post('/send-email', (req, res, next) => {
 router.get('/allexams', (req, res, next) => {
 
   AllExams.find()
-    .populate('pacients')
+    .populate('pacient')
     .then(allFound => {
       res.json(allFound)
-
     })
     .catch(err => {
       res.json(err);
     });
 
 });
+
+router.delete('/exam/:id', (req, res, next) => {
+  const { id } = req.params
+  
+  AllExams.findById(id)
+    .populate('pacient')
+    .then(exam => {
+      const examId = exam.id
+      const model = exam.onModel
+      AllExams.findByIdAndDelete(examId)
+        .then(response => {
+          if(model === "liverExams"){
+            LiverExam.findByIdAndDelete(examId)
+              .then(response => console.log("Exame deletado com sucesso"))
+              .catch(err => console.log(err))
+          }else if(model === "prostateExams"){
+            ProstateExam.findByIdAndDelete(examId)
+              .then(response => console.log(response))
+              .catch(err => console.log(err))
+          }
+        })
+    })
+    .catch(err => {
+      res.json(err);
+    });
+
+});
+
 
 router.get('/oneexam/:id', (req, res, next) => {
 
